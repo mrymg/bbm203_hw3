a5da8f264427a757a11e29b259793a4a8146984d
Yunus Melih Gözütok <ymgozutok@gmail.com>
2018-12-09 16:30:37 +0300

    new

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
 
+struct Node {
+    int data;
+    struct Node *prev;
+    struct Node *next;
+};
+struct Node* head = NULL;
+void insert(int newdata) {
+    struct Node* newnode = (struct Node*) malloc(sizeof(struct Node));
+    newnode->data  = newdata;
+    newnode->prev = NULL;
+    newnode->next = head;
+    if(head !=  NULL)
+        head->prev = newnode ;
+    head = newnode;
+}
+
 int main() {
 
     int x = 10;
